a98fc7f1c6fdd30e752cac7d6c32215d163e217a
Aditya Nidhonkar <adityanidhonkar2004@gmail.com>
2025-01-31 15:11:25 +0530

    minor changes

--- app/api/task/updatetaskstatus/route.js
+++ app/api/task/updatetaskstatus/route.js
@@ -1,23 +1,22 @@
 import { ObjectId } from "mongodb";
-import Connection from "@/database/config"; // Database connection setup
-import Task from "@/models/task"; // Task model using Mongoose
+import Connection from "@/database/config"; 
+import Task from "@/models/task"; 
 
-// Initialize database connection
 Connection();
 
 export async function PUT(request) {
   try {
-    // Parse the incoming request body
+    
     const { taskId, newStatus } = await request.json();
 
-    // Ensure the taskId is a valid ObjectId if you're using Mongoose
+    
     const objectId = new ObjectId(taskId);
 
-    // Use Mongoose to find and update the task
+    
     const updatedTask = await Task.findByIdAndUpdate(
-      objectId, // Task ID as ObjectId
-      { status: newStatus }, // Update status field
-      { new: true } // Return the updated document
+      objectId, 
+      { status: newStatus }, 
+      { new: true } 
     );
 
     if (!updatedTask) {
@@ -26,8 +25,6 @@ export async function PUT(request) {
         { status: 404 }
       );
     }
-
-    // Return the updated task in the response
     return new Response(JSON.stringify(updatedTask), {
       status: 200,
       headers: { "Content-Type": "application/json" },
